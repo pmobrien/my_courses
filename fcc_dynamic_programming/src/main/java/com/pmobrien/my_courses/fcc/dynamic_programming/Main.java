3e6e262c8aab83bfe6a83904b59a9799466f1699
Patrick O'Brien <obrienp@outlook.com>
2021-08-14 23:20:05 -0500

    init

---
+++ fcc_dynamic_programming/src/main/java/com/pmobrien/my_courses/fcc/dynamic_programming/Main.java
@@ -0,0 +1,8 @@
+package com.pmobrien.my_courses.fcc.dynamic_programming;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("hi it's patrick");
+  }
+}
---
+++ fcc_dynamic_programming/src/test/java/com/pmobrien/my_courses/fcc/dynamic_programming/MainTest.java
@@ -0,0 +1,12 @@
+package com.pmobrien.my_courses.fcc.dynamic_programming;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+  @Test
+  public void testMain() {
+    Assertions.assertTrue(true);
+  }
+}
